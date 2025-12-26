f583a632c078a794a4158c9a72a443a8a4152b30
Peter Anyaogu <peter@noble.xyz>
2025-12-26 22:39:12 +0100

    feat(platform): add PlatformSigner for iOS/macOS key management
    Implement secure key generation, signing and management using platform-specific APIs

---
+++ ios/web3_signers/Sources/web3_signers/PlatformSigner.swift
@@ -0,0 +1,191 @@
+import Foundation
+
+#if os(iOS)
+    import Flutter
+#elseif os(macOS)
+    import FlutterMacOS
+#endif
+
+class PlatformSigner: PlatformSignerApi {
+
+    let domain = "PlatformSigner"
+
+    func createKey(keyTag: String, completion: @escaping (Result<[Int64], Error>) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+
+            if self.getSecKey(from: keyTag) != nil {
+                let msg = "Key already exists"
+                let err = NSError(
+                    domain: self.domain, code: -1, userInfo: [NSLocalizedDescriptionKey: msg])
+                DispatchQueue.main.async { completion(.failure(err)) }
+                return
+            }
+
+            var attributes: [String: Any]
+            switch self.createAccessControl() {
+            case .success(let access):
+                attributes = self.createKeyAttributes(keyTag: keyTag, access: access)
+            case .failure(let error):
+                DispatchQueue.main.async { completion(.failure(error)) }
+                return
+            }
+
+            var error: Unmanaged<CFError>?
+            guard let key = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+                let err = error!.takeRetainedValue() as Error
+                DispatchQueue.main.async { completion(.failure(err)) }
+                return
+            }
+
+            let result = self.extractPublicKey(from: key)
+            DispatchQueue.main.async { completion(result) }
+        }
+    }
+
+    func deleteKey(keyTag: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let tag = keyTag.data(using: .utf8)!
+            let query: [String: Any] = [
+                kSecClass as String: kSecClassKey,
+                kSecAttrApplicationTag as String: tag,
+                kSecAttrKeyType as String: kSecAttrKeyTypeEC,
+                kSecMatchLimit as String: kSecMatchLimitOne,
+            ]
+
+            let status = SecItemDelete(query as CFDictionary)
+            guard status == errSecSuccess || status == errSecItemNotFound else {
+                let msg = "Keychain Error"
+                let err = NSError(
+                    domain: self.domain, code: -1, userInfo: [NSLocalizedDescriptionKey: msg])
+                DispatchQueue.main.async { completion(.failure(err)) }
+                return
+            }
+            DispatchQueue.main.async { completion(.success(())) }
+        }
+    }
+
+    func sign(keyTag: String, data: [Int64], completion: @escaping (Result<[Int64], Error>) -> Void)
+    {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+
+            guard let key = self.getSecKey(from: keyTag) else {
+                let msg = "Key not found"
+                let err = NSError(
+                    domain: self.domain, code: -1, userInfo: [NSLocalizedDescriptionKey: msg])
+                DispatchQueue.main.async { completion(.failure(err)) }
+                return
+            }
+
+            let algorithm: SecKeyAlgorithm = .ecdsaSignatureMessageX962SHA256
+            let dataBytes = Data(data.map { UInt8($0) })
+            var error: Unmanaged<CFError>?
+            guard
+                let signature = SecKeyCreateSignature(
+                    key,
+                    algorithm,
+                    dataBytes as CFData,
+                    &error) as Data?
+            else {
+                DispatchQueue.main.async {
+                    completion(.failure(error!.takeRetainedValue() as Error))
+                }
+                return
+            }
+
+            let result = signature.map { Int64($0) }
+            DispatchQueue.main.async { completion(.success(result)) }
+        }
+    }
+
+    func getPublicKey(keyTag: String, completion: @escaping (Result<[Int64]?, Error>) -> Void) {
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+
+            guard let key = self.getSecKey(from: keyTag) else {
+                DispatchQueue.main.async { completion(.success(nil)) }
+                return
+            }
+
+            let result = self.extractPublicKey(from: key)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+
+    private func getSecKey(from keyTag: String) -> SecKey? {
+        let tag = keyTag.data(using: .utf8)!
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrApplicationTag as String: tag,
+            kSecAttrKeyType as String: kSecAttrKeyTypeEC,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnRef as String: true,
+        ]
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess else { return nil }
+        let key = item as! SecKey
+        return key
+    }
+
+    private func extractPublicKey(from privateKey: SecKey) -> Result<[Int64], Error> {
+        guard let publicKey = SecKeyCopyPublicKey(privateKey) else {
+            let msg = "Failed to generate public key from private key"
+            let err = NSError(domain: domain, code: -1, userInfo: [NSLocalizedDescriptionKey: msg])
+            return .failure(err)
+        }
+
+        var error: Unmanaged<CFError>?
+        if let keyData = SecKeyCopyExternalRepresentation(publicKey, &error) as Data? {
+            return .success(keyData.map { Int64($0) })
+        } else {
+            return .failure(error!.takeRetainedValue() as Error)
+        }
+    }
+
+    private func createAccessControl() -> Result<SecAccessControl, Error> {
+        var error: Unmanaged<CFError>?
+        let flags: SecAccessControlCreateFlags = [.privateKeyUsage, .userPresence]
+        let access = SecAccessControlCreateWithFlags(
+            kCFAllocatorDefault,
+            kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+            flags,
+            &error)!
+
+        if let error = error {
+            return .failure(error.takeRetainedValue() as Error)
+        }
+        return .success(access)
+    }
+
+    private func createKeyAttributes(keyTag: String, access: SecAccessControl) -> [String: Any] {
+        var privateKeyAttrs: [String: Any] = [
+            kSecAttrIsPermanent as String: true,
+            kSecAttrApplicationTag as String: keyTag.data(using: .utf8)!,
+            kSecAttrAccessControl as String: access,
+            kSecAttrCanSign as String: true,
+        ]
+
+        var attributes: [String: Any] = [
+            kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+            kSecAttrKeySizeInBits as String: 256,
+            kSecPrivateKeyAttrs as String: privateKeyAttrs,
+        ]
+
+        #if !targetEnvironment(simulator)
+            attributes[kSecAttrTokenID as String] = kSecAttrTokenIDSecureEnclave
+        #endif
+
+        return attributes
+    }
+
+}
